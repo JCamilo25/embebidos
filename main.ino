63801231a4adb53aeb9f4cf4e73a25e0c0f8bf1f
camilo <juanc.gallegog@uqvirtual.edu.co>
2025-02-04 17:15:24 -0500

    Blink

--- main.ino
+++ main.ino
@@ -1,7 +1,13 @@
+// Blink
 void setup()
 {
+    pinMode(Pinled, OUTPUT);
 }
-
+// Encender y apagar el led
 void loop()
 {
+    digitalWrite(Pinled, HIGH);
+    delay(1000);
+    digitalWrite(Pinled, LOW);
+    delay(1000);
 }
